0ee6dbd5c9ac4a2b9770719d04edd8bd84d5005d
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2014-03-23 23:36:38 +0900

    ブロックを生成するようにした

--- HelloSpriteKit/HelloScene.m
+++ HelloSpriteKit/HelloScene.m
@@ -27,4 +27,17 @@
     return self;
 }
 
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    for (UITouch *touch in touches) {
+        // タッチした場所にブロックを生成
+        SKSpriteNode *block = [SKSpriteNode
+                               spriteNodeWithColor:[SKColor yellowColor]
+                               size:CGSizeMake(50,50)];
+        CGPoint location = [touch locationInNode:self];
+        block.position = location;
+        block.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:block.size];
+        [self addChild:block];
+    }
+}
+
 @end
